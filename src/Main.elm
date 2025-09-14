c9068c6cdacf906ddb299f6908efaa133161dfb1
sushidesu <hugemerge@gmail.com>
2025-09-14 18:55:00 +0900

    feat: Show field and add implement of "Multiply"

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,50 @@
 module Main exposing (..)
 
+import Array exposing (Array)
 import Browser
-import Html exposing (Html)
+import Dict exposing (Dict, insert)
+import Html exposing (Html, select)
 import Html.Events exposing (onClick)
 import List exposing (map)
 import Random
+import Random.Extra exposing (maybe)
 import Random.List
+import Random.Set exposing (notInSet)
 
 
 type Card
     = BeFruitful
     | Multiply
-    | FillTheEarth
-    | Dominion
-    | Flourish
-    | GoldenHooves
-    | FallingRock
-    | Wolves
-    | Lightning
-    | Storm
-    | Meteor
-    | Shephion
-    | Plague
-    | Crwoding
-    | Slump
-    | SheepDog
-    | PlanningSheep
-    | AllPurposeSheep
-    | Inspiration
+
+
+
+-- | FillTheEarth
+-- | Dominion
+-- | Flourish
+-- | GoldenHooves
+-- | FallingRock
+-- | Wolves
+-- | Lightning
+-- | Storm
+-- | Meteor
+-- | Shephion
+-- | Plague
+-- | Crwoding
+-- | Slump
+-- | SheepDog
+-- | PlanningSheep
+-- | AllPurposeSheep
+-- | Inspiration
+
+
+beFruitful : List Sheep -> Sheep -> List Sheep
+beFruitful field target =
+    List.append field [ target ]
+
+
+multiply : List Sheep -> List Sheep
+multiply field =
+    List.append field [ Three ]
 
 
 cardToString : Card -> String
@@ -39,56 +56,42 @@ cardToString card =
         Multiply ->
             "増やせよ"
 
-        FillTheEarth ->
-            "地に満ちよ"
 
-        Dominion ->
-            "統率"
 
-        Flourish ->
-            "繁栄"
-
-        GoldenHooves ->
-            "黄金の蹄"
-
-        FallingRock ->
-            "落石"
-
-        Wolves ->
-            "狼"
-
-        Lightning ->
-            "落雷"
-
-        Storm ->
-            "嵐"
-
-        Meteor ->
-            "メテオ"
-
-        Shephion ->
-            "シェフィオン"
-
-        Plague ->
-            "疫病"
-
-        Crwoding ->
-            "過密"
-
-        Slump ->
-            "暴落"
-
-        SheepDog ->
-            "牧羊犬"
-
-        PlanningSheep ->
-            "対策ひつじ"
-
-        AllPurposeSheep ->
-            "万能ひつじ"
-
-        Inspiration ->
-            "霊感"
+-- FillTheEarth ->
+--     "地に満ちよ"
+-- Dominion ->
+--     "統率"
+-- Flourish ->
+--     "繁栄"
+-- GoldenHooves ->
+--     "黄金の蹄"
+-- FallingRock ->
+--     "落石"
+-- Wolves ->
+--     "狼"
+-- Lightning ->
+--     "落雷"
+-- Storm ->
+--     "嵐"
+-- Meteor ->
+--     "メテオ"
+-- Shephion ->
+--     "シェフィオン"
+-- Plague ->
+--     "疫病"
+-- Crwoding ->
+--     "過密"
+-- Slump ->
+--     "暴落"
+-- SheepDog ->
+--     "牧羊犬"
+-- PlanningSheep ->
+--     "対策ひつじ"
+-- AllPurposeSheep ->
+--     "万能ひつじ"
+-- Inspiration ->
+--     "霊感"
 
 
 allCards : List Card
@@ -97,24 +100,25 @@ allCards =
     , BeFruitful
     , BeFruitful
     , Multiply
-    , FillTheEarth
-    , Dominion
-    , Dominion
-    , Flourish
-    , GoldenHooves
-    , FallingRock
-    , Wolves
-    , Lightning
-    , Storm
-    , Meteor
-    , Shephion
-    , Plague
-    , Crwoding
-    , Slump
-    , SheepDog
-    , PlanningSheep
-    , AllPurposeSheep
-    , Inspiration
+
+    -- , FillTheEarth
+    -- , Dominion
+    -- , Dominion
+    -- , Flourish
+    -- , GoldenHooves
+    -- , FallingRock
+    -- , Wolves
+    -- , Lightning
+    -- , Storm
+    -- , Meteor
+    -- , Shephion
+    -- , Plague
+    -- , Crwoding
+    -- , Slump
+    -- , SheepDog
+    -- , PlanningSheep
+    -- , AllPurposeSheep
+    -- , Inspiration
     ]
 
 
@@ -128,11 +132,68 @@ initDeck =
     Random.List.shuffle allCards
 
 
-showCard : Card -> Html msg
+showCard : Card -> Html Msg
 showCard card =
-    Html.div []
-        [ Html.text <| cardToString card
-        ]
+    cardToButton card <| cardToString card
+
+
+cardToButton : Card -> String -> Html Msg
+cardToButton card text =
+    if card == Multiply then
+        Html.button
+            [ onClick UseMultiply
+            ]
+            [ Html.text text
+            ]
+
+    else
+        Html.text text
+
+
+sheepToString : Sheep -> String
+sheepToString sheep =
+    case sheep of
+        One ->
+            "1"
+
+        Three ->
+            "3"
+
+        Ten ->
+            "10"
+
+        Thirty ->
+            "30"
+
+        OneHundred ->
+            "100"
+
+        ThreeHundred ->
+            "300"
+
+        Thousand ->
+            "1000"
+
+
+showSheep : Maybe Sheep -> Bool -> FieldId -> Html Msg
+showSheep maybeSheep selected index =
+    case maybeSheep of
+        Just sheep ->
+            Html.div []
+                [ if selected then
+                    Html.button
+                        [ onClick (ToggleSelectSheep index) ]
+                        [ Html.text "◯" ]
+
+                  else
+                    Html.button
+                        [ onClick (ToggleSelectSheep index) ]
+                        [ Html.text "☓" ]
+                , Html.text <| sheepToString sheep
+                ]
+
+        Nothing ->
+            Html.div [] [ Html.text "-" ]
 
 
 type Sheep
@@ -145,16 +206,38 @@ type Sheep
     | Thousand
 
 
+type alias Field =
+    Array (Maybe Sheep)
+
+
+type alias FieldId =
+    Int
+
+
 type alias Model =
     { count : Int
+    , field : Field
     , hands : List Card
     , deck : List Card
+    , selectedSheep : Dict FieldId Bool
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { count = 0, hands = [], deck = [] }
+    ( { count = 0
+      , field = Array.fromList [ Just One, Nothing, Nothing, Nothing, Nothing ]
+      , hands = []
+      , deck = []
+      , selectedSheep =
+            Dict.fromList
+                [ ( 0, False )
+                , ( 1, False )
+                , ( 2, False )
+                , ( 3, False )
+                , ( 4, False )
+                ]
+      }
     , Cmd.batch [ Random.generate Init initDeck ]
     )
 
@@ -164,6 +247,8 @@ type Msg
     | Decrement
     | Init (List Card)
     | DrawFive
+    | ToggleSelectSheep FieldId
+    | UseMultiply
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -192,6 +277,64 @@ update msg model =
             , Cmd.none
             )
 
+        ToggleSelectSheep fieldId ->
+            ( { model
+                | selectedSheep =
+                    Dict.get fieldId model.selectedSheep
+                        |> (\x ->
+                                case x of
+                                    Just selected ->
+                                        if selected then
+                                            insert fieldId False model.selectedSheep
+
+                                        else
+                                            insert fieldId
+                                                True
+                                                model.selectedSheep
+
+                                    Nothing ->
+                                        model.selectedSheep
+                           )
+              }
+            , Cmd.none
+            )
+
+        UseMultiply ->
+            let
+                updatedField =
+                    findLastNothing model.field
+                        |> Maybe.map (\i -> Array.set i (Just Three) model.field)
+                        |> Maybe.withDefault model.field
+
+                updatedHands =
+                    removeFirst Multiply model.hands
+            in
+            ( { model | field = updatedField, hands = updatedHands }, Cmd.none )
+
+
+findLastNothing : Array (Maybe a) -> Maybe Int
+findLastNothing array =
+    array
+        |> Array.toIndexedList
+        |> List.filter (\( _, v ) -> v == Nothing)
+        -- |> List.reverse
+        |> List.head
+        |> Maybe.map Tuple.first
+
+
+removeFirst : a -> List a -> List a
+removeFirst item list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if x == item then
+                xs
+
+            else
+                x :: removeFirst item xs
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
@@ -202,6 +345,7 @@ view : Model -> Html Msg
 view model =
     Html.div []
         [ Html.button [ onClick DrawFive ] [ Html.text "draw" ]
+        , Html.div [] (Array.toList (Array.indexedMap (\index sheep -> showSheep sheep (Dict.get index model.selectedSheep |> Maybe.withDefault False) index) model.field))
         , Html.div [] (map showCard model.hands)
         ]
 
